54d504a3fb03e9bf927a884eb3f8ddc93c4b5364
itayzafrir <itay.zafrir@arm.com>
2019-01-29 13:16:59 +0200

    DO NOT CHERRY-PICK! PSA_CRYPTO_SECURE + PSA_MAX_PERSISTENT_KEY_IDENTIFIER
    Define PSA_CRYPTO_SECURE in mbed crypto files.
    Expand PSA_MAX_PERSISTENT_KEY_IDENTIFIER from 0xffff0000 to 0xffff0000ffffffff.
    
    This needs a PR in mbed-crypto and needs to be re-imported here via importer.
    PR@mbed-crypto/pull/32

--- features/mbedtls/mbed-crypto/platform/TARGET_PSA/COMPONENT_PSA_SRV_IMPL/psa_crypto_storage.h
+++ features/mbedtls/mbed-crypto/platform/TARGET_PSA/COMPONENT_PSA_SRV_IMPL/psa_crypto_storage.h
@@ -59,7 +59,11 @@ extern "C" {
  * This limitation will probably become moot when we implement client
  * separation for key storage.
  */
+#if defined(MBEDTLS_PSA_CRYPTO_SPM)
+#define PSA_MAX_PERSISTENT_KEY_IDENTIFIER 0xffff000000000000
+#else
 #define PSA_MAX_PERSISTENT_KEY_IDENTIFIER 0xffff0000
+#endif
 
 /**
  * \brief Format key data and metadata and save to a location for given key
