3bc5f9086c3bbf1471906d6a4753457cf46f19cf
itayzafrir <itay.zafrir@arm.com>
2019-02-13 10:16:49 +0200

    Add license

--- components/TARGET_PSA/services/crypto/COMPONENT_SPE/crypto_types_spe.h
+++ components/TARGET_PSA/services/crypto/COMPONENT_SPE/crypto_types_spe.h
@@ -14,7 +14,7 @@
  * This header file does not declare any function.
  */
 /*
- *  Copyright (C) 2018, ARM Limited, All Rights Reserved
+ *  Copyright (C) 2019, ARM Limited, All Rights Reserved
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
